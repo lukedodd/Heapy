118ea91eafeb3b58fce259c9cc1d1fec7a19163d
luke.dodd <luke.dodd@hawkeyeinnovations.com>
2020-03-26 19:37:09 +0000

    Proper command line argument escaping

--- Heapy/Heapy.cpp
+++ Heapy/Heapy.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <codecvt>
 
 typedef NTSTATUS (NTAPI *pfnNtQueryInformationProcess)(
 	IN  HANDLE ProcessHandle,
@@ -175,12 +176,102 @@ DWORD LoadLibraryInjection(HANDLE proc, const char *dllName){
 	return dwThreadExitCode;
 }
 
-std::string getDirectoryOfFile(const std::string &file){
-	size_t pos = (std::min)(file.find_last_of("/"), file.find_last_of("\\"));
+std::wstring getDirectoryOfFile(const std::wstring &file){
+	size_t pos = (std::min)(file.find_last_of(L"/"), file.find_last_of(L"\\"));
 	if(pos == std::string::npos)
-		return ".";
+		return L".";
 	else
 		return file.substr(0, pos);
+}
+
+/*++
+
+Routine Description:
+
+	This routine appends the given argument to a command line such
+	that CommandLineToArgvW will return the argument string unchanged.
+	Arguments in a command line should be separated by spaces; this
+	function does not add these spaces.
+
+Arguments:
+
+	Argument - Supplies the argument to encode.
+
+	CommandLine - Supplies the command line to which we append the encoded argument string.
+
+	Force - Supplies an indication of whether we should quote
+			the argument even if it does not contain any characters that would
+			ordinarily require quoting.
+
+From: https://docs.microsoft.com/en-gb/archive/blogs/twistylittlepassagesallalike/everyone-quotes-command-line-arguments-the-wrong-way
+
+--*/
+
+void ArgvQuote(const std::wstring& Argument, std::wstring& CommandLine, bool Force){
+	//
+	// Unless we're told otherwise, don't quote unless we actually
+	// need to do so --- hopefully avoid problems if programs won't
+	// parse quotes properly
+	//
+
+	if (Force == false &&
+		Argument.empty() == false &&
+		Argument.find_first_of(L" \t\n\v\"") == Argument.npos)
+	{
+		CommandLine.append(Argument);
+	}
+	else {
+		CommandLine.push_back(L'"');
+
+		for (auto It = Argument.begin(); ; ++It) {
+			unsigned NumberBackslashes = 0;
+
+			while (It != Argument.end() && *It == L'\\') {
+				++It;
+				++NumberBackslashes;
+			}
+
+			if (It == Argument.end()) {
+
+				//
+				// Escape all backslashes, but let the terminating
+				// double quotation mark we add below be interpreted
+				// as a metacharacter.
+				//
+
+				CommandLine.append(NumberBackslashes * 2, L'\\');
+				break;
+			}
+			else if (*It == L'"') {
+
+				//
+				// Escape all backslashes and the following
+				// double quotation mark.
+				//
+
+				CommandLine.append(NumberBackslashes * 2 + 1, L'\\');
+				CommandLine.push_back(*It);
+			}
+			else {
+
+				//
+				// Backslashes aren't special here.
+				//
+
+				CommandLine.append(NumberBackslashes, L'\\');
+				CommandLine.push_back(*It);
+			}
+		}
+
+		CommandLine.push_back(L'"');
+	}
+}
+
+std::string ws2s(const std::wstring& wstr){
+	using convert_typeX = std::codecvt_utf8<wchar_t>;
+	std::wstring_convert<convert_typeX, wchar_t> converterX;
+
+	return converterX.to_bytes(wstr);
 }
 
 extern "C" int main(int argc, char* argv[]){
@@ -192,73 +283,101 @@ extern "C" int main(int argc, char* argv[]){
 			 
 		return -1;
 	}
-	char *injectionTarget = argv[1];
 
 	bool win64 = false;
 	#ifdef _WIN64
 		win64 = true;
 	#endif
 	// Select correct dll name depending on whether x64 or win32 version launched.
-	std::string heapyInjectDllName;
+	std::wstring heapyInjectDllName;
 	if(win64)
-		heapyInjectDllName = "HeapyInject_x64.dll";
+		heapyInjectDllName = L"HeapyInject_x64.dll";
 	else
-		heapyInjectDllName = "HeapyInject_Win32.dll";
+		heapyInjectDllName = L"HeapyInject_Win32.dll";
 
 	// Assume that the injection payload dll is in the same directory as the exe.
-	CHAR exePath[MAX_PATH];
-	GetModuleFileNameA(NULL, exePath, MAX_PATH );
-	std::string dllPath = getDirectoryOfFile(std::string(exePath)) + "\\" + heapyInjectDllName;
+	wchar_t exePath[MAX_PATH];
+	GetModuleFileNameW(NULL, exePath, MAX_PATH );
 
-	std::string commandLine;
-	commandLine += "\"";
-	commandLine += injectionTarget;
-	commandLine += "\"";
-	for(int i = 2; i < argc; ++i){
-		commandLine += " " + std::string(argv[i]);
+	std::wstring dllPath = getDirectoryOfFile(std::wstring(exePath)) + L"\\" + heapyInjectDllName;
+
+	std::wstring commandLine;
+
+	//
+	// Form command line...
+	// 
+	{
+		wchar_t **wArgList;
+		int nArgs;
+		wArgList = CommandLineToArgvW(GetCommandLineW(), &nArgs);
+
+
+		commandLine += L"\"";
+		commandLine += std::wstring(wArgList[1]);
+		commandLine += L"\"";
+		for (int i = 2; i < nArgs; ++i) {
+
+			std::wstring argQuoted;
+
+			ArgvQuote(std::wstring(wArgList[i]), argQuoted, false);
+
+			std::wcout << argQuoted << "\n";
+
+			commandLine += L" " + argQuoted;
+		}
+
+		std::wcout << commandLine << "\n";
+		LocalFree(wArgList);
 	}
 
 	// Start our new process with a suspended main thread.
 	std::cout << "Starting process with heap profiling enabled..." << std::endl;
-	std::cout << "Target exe path: " << injectionTarget << std::endl;
-	std::cout << "Target execommand line: " << commandLine << std::endl;
-	std::cout << "Dll to inject: " << dllPath << std::endl;
+	std::wcout << "Target exe command line: " << commandLine << std::endl;
+	std::wcout << "Dll to inject: " << dllPath << std::endl;
+
 
 
 	DWORD flags = CREATE_SUSPENDED;
 	PROCESS_INFORMATION pi;
-	STARTUPINFOA si;
-	GetStartupInfoA(&si);
+	STARTUPINFOW si;
+
+	GetStartupInfoW(&si);
 
 	// CreatePRocessA can modify input arg so do this to be safe.
-	std::vector<char> commandLineMutable(commandLine.begin(), commandLine.end()); 
+	std::vector<wchar_t> commandLineMutable(commandLine.begin(), commandLine.end()); 
 	commandLineMutable.push_back('\0');
 
-	if(CreateProcessA(NULL, commandLineMutable.data(), NULL, NULL, 0, flags, NULL, 
+	if(CreateProcessW(NULL, commandLineMutable.data(), NULL, NULL, 0, flags, NULL, 
 					  NULL, &si, &pi) == 0){
-		std::cerr << "Error creating process " << injectionTarget << std::endl;
+		std::wcerr << "Error creating process, command line " << commandLine << std::endl;
+		Sleep(10000);
 		return -1;
 	}
 
 	ProcessStartContext ProcessContext;
-	if (!WaitForProcessStart(pi.hProcess, pi.hThread, &ProcessContext))
+	if (!WaitForProcessStart(pi.hProcess, pi.hThread, &ProcessContext)) {
+		Sleep(10000);
 		return -1;
+	}
 
 	// Inject our dll.
 	// This method returns only when injection thread returns.
 	try{
-		if(!LoadLibraryInjection(pi.hProcess, dllPath.c_str())){
+		// TODO: We should really upgrade LoadLibraryInjection to handle wstrings...
+		auto dllPathNormalString = ws2s(dllPath);
+		if(!LoadLibraryInjection(pi.hProcess, dllPathNormalString.c_str())){
 			throw std::runtime_error("LoadLibrary failed!");
 		}
 	}catch(const std::exception &e){
 		std::cerr << "\n";
 		std::cerr << "Error while injecting process: " << e.what() << "\n\n";
-		std::cerr << "Check that the hook dll (" << dllPath << " is in the correct location.\n\n";
+		std::wcerr << "Check that the hook dll (" << dllPath << " is in the correct location.\n\n";
 		std::cerr << "Are you trying to inject a " << (win64 ? " 32 bit " : " 64 bit ") << " application using the "
 			<<  (win64 ? " 64 bit " : " 32 bit ") << " injector?\n\n";
 
 		// TODO: figure out how to terminate thread. This does not always work.
 		TerminateProcess(pi.hProcess, 0);
+		Sleep(10000);
 		return -1;
 	}
 
